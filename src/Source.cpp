889009f1adeeea4921735ab54057ab17ced5e2ed
Fahersto <31221314+Fahersto@users.noreply.github.com>
2021-07-26 12:50:09 +0200

    added test executable

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,6 +1,27 @@
-#include "Hook.h"
+#include <cstdio>
+
+#include "MidfunctionHook.h"
+
+int square(int x)
+{
+	return x * x;
+}
+
 
 int main()
 {
+	int x = 10;
+	printf("square of %d is %d\n", x, square(x));
 
+	int8_t* aslr = (int8_t*)GetModuleHandle(NULL);
+	hookftw::MidfunctionHook squareHook;
+	squareHook.Hook(aslr+ 0x2000,
+		[](hookftw::context* ctx) {
+
+			// change first parameter to 7 (fastcall)
+			ctx->rcx = 7;
+		}
+	);
+
+	printf("square of %d is %d\n", x, square(x));
 }
