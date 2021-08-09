49e2cfcd09588e3033587fc8cbbdff122c7520bf
Fahersto <31221314+Fahersto@users.noreply.github.com>
2021-08-09 18:04:38 +0200

    adding all available functionality

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,27 +1,120 @@
+/**
+* This example project is meant to be injected into "victim.exe" provided in the hookftw_example/target folder in this repository. 
+* Debug symbols of "victim.exe" are also provided.
+* The project demonstrates all available hooking techniques in hookFTW.
+*/
+
 #include <cstdio>
+#include <Windows.h>
 
+#include "Detour.h"
 #include "MidfunctionHook.h"
+#include "IATHook.h"
+#include "VFTHook.h"
+#include "VEHHook.h"
 
-int square(int x)
+#include "DbgSymbols.h"
+
+
+// type definition of Sleep function (MSDN)
+using originalSleepFunction = void(WINAPI*)(DWORD dwMilliseconds);
+originalSleepFunction originalSleep;
+
+void WINAPI hookedSleep(DWORD dwMilliseconds)
 {
-	return x * x;
+	printf("Sleep was called!\n");
+
+	// call original Sleep function
+	originalSleep(dwMilliseconds);
 }
 
-
-int main()
+void hookedCow()
 {
-	int x = 10;
-	printf("square of %d is %d\n", x, square(x));
+	printf("\t[Cow] - hooked - makes muuuh\n");
+}
 
-	int8_t* aslr = (int8_t*)GetModuleHandle(NULL);
-	hookftw::MidfunctionHook squareHook;
-	squareHook.Hook(aslr+ 0x2000,
+int hookedCalculate(int x)
+{
+	printf("calculate called, returning %d\n", x);
+	return x;
+}
+
+int __fastcall hookedCdeclCallFunc(int x, int y, int z)
+{
+	printf("cdeclCallFunc called\n");
+	return x;
+}
+
+DWORD __stdcall Run(LPVOID hModule)
+{
+	int8_t* baseAddress = (int8_t*)GetModuleHandle(NULL);
+
+	// byte patching at the beginning of the function
+	hookftw::Detour detourHook;
+	detourHook.Hook(baseAddress + 0x1B60, (int8_t*)hookedCdeclCallFunc);
+
+	// byte patching at the beginning of the function
+	hookftw::MidfunctionHook midfunctionHook;
+	midfunctionHook.Hook(
+		baseAddress + 0x1B20,
 		[](hookftw::context* ctx) {
+			// print registers
+			ctx->PrintRegister();	
 
-			// change first parameter to 7 (fastcall)
-			ctx->rcx = 7;
+			// modify return value to the result of a call of the function with different parameters
+			ctx->rax = ctx->CallOriginal<int>(hookftw::CallingConvention::fastcall_call, 2, 3, 5);
+
+			// skip original invocation of function
+			ctx->SkipOriginalFunction();
 		}
 	);
 
-	printf("square of %d is %d\n", x, square(x));
+	// import address table hook
+	hookftw::IATHook iatHook;
+	originalSleep = (originalSleepFunction)iatHook.Hook("Kernel32.dll", "Sleep", (int8_t*)hookedSleep);
+
+	// use dbgsymbols (.pdb file) to resolve virtual function table of Cow class
+	hookftw::DbgSymbols dbgSymbols;
+	// hook the first virtual method of the Cow class
+	hookftw::VFTHook vftHook((int8_t**)dbgSymbols.GetAddressBySymbolName("Cow::`vftable'"));
+	vftHook.Hook(0, (int8_t*)hookedCow);
+
+	// vectored exception ahndler hook
+	hookftw::VEHHook vehHook;
+	vehHook.Hook(baseAddress + 0x1AA0, (int8_t*)hookedCalculate);
+
+	while (true)
+	{
+		// press F1 to unhook all hooks
+		if (GetAsyncKeyState(VK_F1) & 0x1)
+		{
+			detourHook.Unhook();
+			midfunctionHook.Unhook();
+			iatHook.Unhook();
+			vftHook.Unhook();
+			vehHook.Unhook();
+			break;
+		}
+		Sleep(1);
+	}
+
+	// clean up. All hooks have to be removed at this point or the target application will crash.
+	FreeLibraryAndExitThread(static_cast<HMODULE>(hModule), 0);
+	return TRUE;
+}
+
+BOOL __stdcall DllMain(HINSTANCE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hModule);
+		CreateThread(nullptr, 0, Run, hModule, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	default:
+		break;
+	}
+	return TRUE;
 }
